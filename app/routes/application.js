d144dedb879307d9eb396fed829ef89f5e405f63
jswob <jswob@jswob-lenovo-legion jacswob@gmail.com>
2019-07-14 10:16:53 +0200

    Create image background

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,9 +1,4 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-    model(){
-        return this.store.queryRecord("weather", {
-            id: 7532559
-        });
-    }
 });
