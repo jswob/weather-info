abd2bb87962baec3508b3668b38de347802c7de6
jswob <jswob@jswob-lenovo-legion jacswob@gmail.com>
2019-07-24 22:29:53 +0200

    Save location as a model in ember-data

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,31 +1,32 @@
 import Component from "@ember/component";
-import $ from "jquery";
 import { inject as service } from "@ember/service";
 
 export default Component.extend({
   media: service(),
+  store: service(),
   tagName: "nav",
   classNames: ["nav-bar", "layout-row"],
-  fullAddress: null,
-  googleAuto: null,
 
   actions: {
-    done() {
-      $("#message")
-        .fadeOut(500, function(){
-          this.set("message", "Focus out");
-        })
-        .fadeIn(500);
-    },
-    placeChanged(place) {
-      this.set("placeJSON", JSON.stringify(place, undefined, 2));
-      this.set("googleAuto", "done");
-      if (place.adr_address) {
-        let regexp = /(<span(?: \w+="[^"]+")*(?: \w+="[^"]+")*>([^<]*)<\/span>)/g,
-          fullAddress = place.adr_address.replace(regexp, "$2");
-        this.set("cleanFullAddress", fullAddress);
+    placeChanged(data) {
+      const place = JSON.parse(JSON.stringify(data, undefined, 2));
+      const localization = {};
+      if(!place.address_components){
+        return this.set("errorMessage", "Could not specify locality for that place.");
       }
-      this.set("fullAddress", place.adr_address);
+      place.address_components.forEach((addressComponent) => {
+        if(addressComponent.types[0] === "locality"){
+          localization.city = addressComponent.long_name;
+        }
+        if(addressComponent.types[0] === "country"){
+          localization.country = addressComponent.short_name;
+        }
+      })
+      if(!localization.city || !localization.country){
+        return this.set("errorMessage", "Could not specify locality for that place.");
+      }
+      return this.store.createRecord("localization", localization);
+
     }
   }
 });
--- app/models/localization.js
+++ app/models/localization.js
@@ -2,7 +2,6 @@ import DS from 'ember-data';
 const { Model, attr } = DS;
 
 export default Model.extend({
-    address: attr("string"),
     city: attr("string"),
     country: attr("string")
 });
