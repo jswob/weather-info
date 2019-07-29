43481438656351acd67c8947b31cb3d3b74de68c
jacek swoboda <jacswob@gmail.com>
2019-07-29 19:54:19 +0200

    Create createUser action in x-form/sign-up component

--- app/components/x-form/sign-up.js
+++ app/components/x-form/sign-up.js
@@ -1,10 +1,19 @@
 import Component from "@ember/component";
+import { inject as service } from "@ember/service";
 
 export default Component.extend({
+  store: service(),
   tagName: "",
+  init() {
+    this._super(...arguments);
+    return this.set("user", this.store.createRecord("user"));
+  },
+  willDestroyElement() {
+    return this.get("user").unloadRecord();
+  },
   actions: {
-      createUser(){
-          console.log("User was succesfully created");
-      }
+    createUser() {
+      return this.get("user").save();
+    }
   }
 });
