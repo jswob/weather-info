580db79c8cc044f8a27bf8958e8209de4b0b6f08
jacek swoboda <jacswob@gmail.com>
2019-07-29 19:40:50 +0200

    Create user model validaton via ember-cli-validations

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,34 @@
 import DS from "ember-data";
+import {buildValidations, validator} from 'ember-cp-validations';
 const { Model, attr, hasMany } = DS;
 
-export default Model.extend({
+const Validations = buildValidations({
+  email: [
+    validator("presence", {
+      presence: true,
+      message: "Email can not be empty"
+    }),
+    validator("format", {
+      type: "email"
+    }),
+  ],
+  password: [
+    validator("presence", {
+      presence: true,
+      message: "Password can not be empty"
+    }),
+    validator('format', {
+      regex: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).*$/,
+      message: 'Password must include at least one upper case letter, one lower case letter, and a number'
+    }),
+    validator("length", {
+      min: 6,
+      max: 40
+    })
+  ]
+});
+
+export default Model.extend(Validations, {
   email: attr("string"),
   password: attr("string"),
   localizations: hasMany("localization"),
