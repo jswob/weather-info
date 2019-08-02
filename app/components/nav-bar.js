d8b7307ae96bafe425b91c8d7401021d526188b1
jacek swoboda <jacswob@gmail.com>
2019-08-02 20:03:56 +0200

    Create nav-bar component

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,47 +1,19 @@
 import Component from "@ember/component";
-import { inject as service } from "@ember/service";
-import { computed } from "@ember/object";
 
 export default Component.extend({
-  media: service(),
-  store: service(),
   tagName: "nav",
-  classNames: ["nav-bar", "layout-row"],
-
-  inputClasses: computed("errorMessage", function() {
-    let classes = "main-input-container flex-95 layout-row ";
-    if(this.get("errorMessage")) {
-      classes += "error";
-    }
-    return classes;
-  }),
-
+  classNames: ["nav-bar layout-row layout-align-center-center"],
+  localization: 0,
   actions: {
-    placeChanged(data) {
-      const place = JSON.parse(JSON.stringify(data, undefined, 2));
-      const localization = {};
-      if (!place.address_components) {
+    searchForForecast() {
+      const localization = this.get("localization");
+      if (!localization.id) {
         return this.set(
           "errorMessage",
-          "Could not specify locality for that place."
+          "You need to specify valid localization if you want to find forecast"
         );
       }
-      place.address_components.forEach(addressComponent => {
-        if (addressComponent.types[0] === "locality") {
-          localization.city = addressComponent.long_name;
-        }
-        if (addressComponent.types[0] === "country") {
-          localization.country = addressComponent.short_name;
-        }
-      });
-      if (!localization.city || !localization.country) {
-        return this.set(
-          "errorMessage",
-          "Could not specify locality for that place."
-        );
-      }
-      this.set("errorMessage", null);
-      return this.store.createRecord("localization", localization);
+      this.transitionToForecast(localization.id);
     }
   }
 });
