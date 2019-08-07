156633fdc86646061a693aa7f209cd044c745852
jacek swoboda <jacswob@gmail.com>
2019-08-07 18:06:39 +0200

    Create route forecast

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
-  this.route('forecast');
+  this.route('forecast', {path: "/:localization_id"});
 });
 
 export default Router;
---
+++ app/routes/forecast.js
@@ -0,0 +1,11 @@
+import Route from "@ember/routing/route";
+
+export default Route.extend({
+  async model({ localization_id }) {
+    const localization = await this.store.findRecord("localization", localization_id);
+    return this.store.query("forecast", {
+      city: localization.get("city"),
+      country: localization.get("country")
+    });
+  }
+});
