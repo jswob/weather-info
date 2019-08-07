2c084ec2be627680797805914cd6e4aac5ef8ad0
jacek swoboda <jacswob@gmail.com>
2019-08-07 18:36:36 +0200

    Create acceptance application test

--- tests/acceptance/application-test.js
+++ tests/acceptance/application-test.js
@@ -1,9 +1,27 @@
-import { module, test, beforeEach } from "qunit";
-import { visit, click, currentURL, fillIn } from "@ember/test-helpers";
+import { module, test } from "qunit";
+import {
+  visit,
+  click,
+  currentURL,
+  fillIn,
+  pauseTest,
+  resumeTest
+} from "@ember/test-helpers";
 import { setupApplicationTest } from "ember-qunit";
 
 module("Acceptance | application", function(hooks) {
   setupApplicationTest(hooks);
 
-  test("Transition to forecast route after creating localization model", async function(assert) {});
+  test("Transition to forecast route after creating localization model", async function(assert) {
+    await visit("/");
+    assert.equal(currentURL(), "/", "ends in forecast");
+    await fillIn("input", "Ali");
+    await setTimeout(() => resumeTest(), 600);
+    await pauseTest();
+    await click("li.ember-power-select-option");
+    await setTimeout(() => resumeTest(), 200);
+    await pauseTest();
+    await click("button.md-icon-button");
+    assert.notEqual(currentURL(), "/", "ends in forecast");
+  });
 });
