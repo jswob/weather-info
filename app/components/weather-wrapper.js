5de4a9305293d01ad48969efdce2fbe74b7a2b80
jacek swoboda <jacswob@gmail.com>
2019-09-07 11:54:10 +0200

    Create scss for weather-wrapper component

--- app/components/weather-wrapper.js
+++ app/components/weather-wrapper.js
@@ -1,9 +1,10 @@
 import Component from "@ember/component";
 
 export default Component.extend({
+  classNames: ["weather-wrapper"],
+  isCelsius: true,
   didReceiveAttrs() {
     this._super(...arguments);
     this.set("selectedForecast", this.get("forecasts").toArray()[0]);
   },
-  isCelsius: true
 });
